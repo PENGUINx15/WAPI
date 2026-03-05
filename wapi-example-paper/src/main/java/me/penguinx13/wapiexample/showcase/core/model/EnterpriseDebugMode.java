42258ab733977066e774a5080bf4716918adcad4
PENGUINx13 <demchuk250706@gmail.com>
2026-03-05 23:47:42 +0100

    05.03.2026 - v2.0.0

--- wapi-example-paper/src/main/java/me/penguinx13/wapiexample/showcase/core/model/EnterpriseDebugMode.java
+++
@@ -1,7 +0,0 @@
-package me.penguinx13.wapiexample.showcase.core.model;
-
-public enum EnterpriseDebugMode {
-    OFF,
-    BASIC,
-    TRACE
-}
--- wapi-example-paper/src/main/java/me/penguinx13/wapiexample/showcase/core/model/EnterpriseUser.java
+++
@@ -1,4 +0,0 @@
-package me.penguinx13.wapiexample.showcase.core.model;
-
-public record EnterpriseUser(String name, int age) {
-}
